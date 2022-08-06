6da795165a67f1c0d6256bc84db4685766ec2dc1
MoldyLunchBox <duu116@gmail.com>
2022-08-06 04:21:23 +0100

    back-end done for now, creating the react app to start making requests to the backend

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,32 +1,15 @@
 const express = require('express')
-const Workout = require('../models/workouts')
+const {createWorkout ,getAllWorkouts, getWorkout, deleteWorkout, updateWorkout,} = require('../controllers/workoutController')
 const router = express.Router()
 // get all workouts
-router.get('/', (req, res) => {
-    res.json({mssg: 'GET all workouts'})
-})
+router.get('/', getAllWorkouts)
 // get a single workout
-router.get('/:id', (req, res)=>{
-    res.json({mssg: 'GET a single workout'})
-})
-
+router.get('/:id', getWorkout)
 // POST a new workout
-router.post('/', async (req, res) => {
-    const {title, load, reps} = req.body
-    try {
-        const workout = await Workout.create({title, load, reps})
-        res.status(200).json(workout)
-    }
-    catch (err){
-        res.status(400).json({error: err.message})
-    }
-})
+router.post('/', createWorkout)
+
 // DELETE a  workout
-router.delete('/:id', (req, res) => {
-    res.json({mssg: 'delete a  workout'})
-})
+router.delete('/:id', deleteWorkout)
 // UPDATE a workout
-router.patch('/:id', (req, res) => {
-    res.json({mssg: 'update all workouts'})
-})
+router.patch('/:id', updateWorkout)
 module.exports = router
