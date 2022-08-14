a6326036d8ed3bcfa3c846dd345361f9005bd946
MoldyLunchBox <duu116@gmail.com>
2022-08-14 07:46:50 +0100

    kok

--- front-end/src/context/WorkoutContext.js
+++ front-end/src/context/WorkoutContext.js
@@ -1,11 +1,19 @@
-import {createContext} from 'react'
+
+import {createContext, useReducer} from 'react'
 
 export const WorkoutContext = createContext()
-
+export const workoutsReducer = (state, action) => {
+     
+}
 export const WorkoutsContextProvider = ({children}) =>{
+    const [state, dispatch] = useReducer(workoutsReducer, {
+        workouts: null
+    })
+
     return (
         <WorkoutContext.Provider>
-            
+            {children}
         </WorkoutContext.Provider>
     )
 }
+
